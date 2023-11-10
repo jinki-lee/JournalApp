e3d042d4f92c090d5b990f6ce6c57ad92ad265f7
Jinki Lee <JINKILEE552@GMAIL.COM>
2023-11-10 11:43:56 -0700

    revert changes

--- app/Navbar.js
+++ app/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
     <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center">
-          <Image
+          <img
             src="https://flowbite.com/docs/images/logo.svg"
             className="h-8 mr-3"
             alt="Flowbite Logo"
