acc38f49ce879ee5f033250d8284af01ebf9d6c1
Jinki Lee <JINKILEE552@GMAIL.COM>
2023-11-10 12:36:52 -0700

    Update firebase.js

--- app/week10/_utils/firebase.js
+++ app/week10/_utils/firebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
