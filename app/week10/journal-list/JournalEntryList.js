9ee762e1f914b19f9de2b10a160f848d33e2a611
Jinki Lee <JINKILEE552@GMAIL.COM>
2023-11-10 17:23:01 -0700

    changed filter system

--- app/week10/journal-list/JournalEntryList.js
+++ app/week10/journal-list/JournalEntryList.js
@@ -3,54 +3,73 @@ import Heading from "./heading";
 import JournalEntry from "./JournalEntry";
 
 export default function JournalEntryList({ entries, onEntrySelect }) {
-  const [startDateFilter, setStartDateFilter] = useState("");
-  const [endDateFilter, setEndDateFilter] = useState("");
+  const [selectedMonth, setSelectedMonth] = useState("");
+  const [selectedYear, setSelectedYear] = useState("");
 
   let filteredEntries = useMemo(() => {
     let tempEntries = [...entries];
 
-    if (startDateFilter) {
-      tempEntries = tempEntries.filter(
-        (entry) => entry.date >= new Date(startDateFilter)
-      );
+    if (selectedMonth) {
+      tempEntries = tempEntries.filter((entry) => {
+        const entryDate = new Date(entry.date);
+        return entryDate.getMonth() + 1 === selectedMonth;
+      });
     }
 
-    if (endDateFilter) {
-      tempEntries = tempEntries.filter(
-        (entry) => entry.date <= new Date(endDateFilter)
-      );
+    if (selectedYear) {
+      tempEntries = tempEntries.filter((entry) => {
+        const entryDate = new Date(entry.date);
+        return entryDate.getFullYear() === selectedYear;
+      });
     }
 
     return tempEntries;
-  }, [entries, startDateFilter, endDateFilter]);
+  }, [entries, selectedMonth, selectedYear]);
 
   return (
     <>
       <div className="mx-4 sm:mx-0 flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4 items-center">
         <div className="flex-grow sm:w-1/3 w-full">
-          <label htmlFor="startDate" className="self-center">
-            Filter by Start Date:
+          <label htmlFor="selectMonth" className="self-center">
+            Filter by Month:
           </label>
-          <input
-            id="startDate"
-            type="date"
-            value={startDateFilter}
-            onChange={(e) => setStartDateFilter(e.target.value)}
+          <select
+            id="selectMonth"
+            value={selectedMonth}
+            onChange={(e) => setSelectedMonth(parseInt(e.target.value))}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
+          >
+            <option value="">All Months</option>
+            <option value="1">January</option>
+            <option value="2">February</option>
+            <option value="3">March</option>
+            <option value="4">April</option>
+            <option value="5">May</option>
+            <option value="6">June</option>
+            <option value="7">July</option>
+            <option value="8">August</option>
+            <option value="9">September</option>
+            <option value="10">October</option>
+            <option value="11">November</option>
+            <option value="12">December</option>
+          </select>
         </div>
 
         <div className="flex-grow sm:w-1/3 w-full">
-          <label htmlFor="endDate" className="self-center">
-            Filter by End Date:
+          <label htmlFor="selectYear" className="self-center">
+            Filter by Year:
           </label>
-          <input
-            id="endDate"
-            type="date"
-            value={endDateFilter}
-            onChange={(e) => setEndDateFilter(e.target.value)}
+          <select
+            id="selectYear"
+            value={selectedYear}
+            onChange={(e) => setSelectedYear(parseInt(e.target.value))}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          />
+          >
+            <option value="">All Years</option>
+            <option value="2023">2023</option>
+            <option value="2022">2022</option>
+            {/* Add options for other years */}
+          </select>
         </div>
       </div>
 
