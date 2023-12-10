d8414eabcd979c1b5f3114dd9101c04145c28a6a
Jinki Lee <JINKILEE552@GMAIL.COM>
2023-12-10 13:18:46 -0700

    updated modal.

--- app/Journal/journal-list/JournalModal.js
+++ app/Journal/journal-list/JournalModal.js
@@ -4,60 +4,57 @@ const JournalModal = ({ isOpen, entry, onClose }) => {
   if (!isOpen || !entry) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-filter backdrop-blur-sm">
-      <div className="relative p-4 w-full max-w-lg bg-white rounded-lg shadow dark:bg-gray-700">
-        {/* Modal Header */}
-        <div className="flex items-center justify-between p-4 border-b rounded-t dark:border-gray-600">
-          <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-            {entry.date}
-            <br />
-          </h3>
-          <button
-            onClick={onClose}
-            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-2 dark:hover:bg-gray-600 dark:hover:text-white"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 inline-block align-text-top"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </svg>{" "}
-          </button>
-        </div>
-        {/* Modal Body */}
-        <div className="p-4 space-y-4">
-          <p className="text-base leading-relaxed text-gray-900 dark:text-gray-400">
-            Title: {entry.title}
-            <br />
-            Content: {entry.content}
-          </p>
-          {entry.songTitle && entry.songTitle.length > 0 && (
+    <div className="overflow-y-auto overflow-x-hidden fixed inset-0 z-50 flex items-center justify-center backdrop-filter backdrop-blur-sm">
+      <div className="relative p-4 w-full max-w-xl">
+        <div className="relative bg-white rounded-lg shadow dark:bg-gray-800 p-4 sm:p-5">
+          <div className="flex justify-between mb-4 rounded-t sm:mb-5">
             <div>
-              <h4 className="font-semibold">Songs:</h4>
-              <ul>
-                {entry.songTitle.map((link, index) => (
-                  <li key={index}>{link}</li>
-                ))}
-              </ul>
+              <h3 className="text-lg text-gray-900 dark:text-white md:text-xl font-semibold">
+                {entry.title}
+              </h3>
+
             </div>
-          )}
-        </div>
-        {/* Modal Footer */}
-        <div className="flex items-center p-4 border-t border-gray-200 rounded-b dark:border-gray-600">
-          <button
-            onClick={onClose}
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Close
-          </button>
+            <button
+              onClick={onClose}
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 inline-flex dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+          </div>
+          <p className="font-bold">{entry.date}</p>
+                          {/* Songs List */}
+            {entry.songTitle && entry.songTitle.length > 0 && (
+              <div className="mb-5 mt-5">
+                <h4 className="font-semibold">Songs:</h4>
+                <ul>
+                  {entry.songTitle.map((song, index) => (
+                    <li key={index}>{song}</li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          <div className="overflow-auto max-h-80">
+            {" "}
+            {/* Set a maximum height as needed */}
+            <h4 className="font-semibold">Journal:</h4>
+
+            <p className="text-base leading-relaxed text-gray-900 dark:text-gray-400">
+              {entry.content}
+            </p>
+          </div>
         </div>
       </div>
     </div>
